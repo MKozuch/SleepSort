5ae2c340829722eb184deb5ae8748f88fd4f8369
Michal Kozuch <michal.kozuch@yahoo.com>
2023-01-17 11:49:51 +0100

    initial commit

---
+++ SleepSort.cpp
@@ -0,0 +1,44 @@
+﻿#include "SleepSort.h"
+
+#include <iostream>
+
+unsigned intRepr(const Widget& w)
+{
+   return w.i;
+}
+unsigned intRepr(const std::string& s)
+{
+   if (s.empty()) return 0;
+   return static_cast<unsigned>(s[0]);
+}
+
+Widget::Widget(unsigned i) : i(i)
+{};
+
+
+int main()
+{
+   std::vector<Widget> v;
+   for (auto i : { 6,5,4,3,2,160,50,40,30,20,10 })
+      v.emplace_back(i);
+
+   auto sorted = sleepSort(v);
+   for (auto w : sorted)
+      std::cout << intRepr(w) << ", ";
+   std::cout << std::endl;
+
+   std::vector<int> v1{ 6,5,4,3,2,160,50,40,30,20,10 };
+   auto sorted1 = sleepSort(v1);
+   for (auto w : sorted1)
+      std::cout << w << ", ";
+   std::cout << std::endl;
+
+   std::vector<std::string> v2{ "6","5","4","3","2","160","50","40","30","20","10" };
+   auto sorted2 = sleepSort(v2);
+   for (auto w : sorted2)
+      std::cout << w << ", ";
+   std::cout << std::endl;
+
+   // std::vector<Widget2> v3(5);
+   // auto sorted3 = sleepSort(v3); // the associated constraints are not satisfied
+}
---
+++ SleepSort.h
@@ -0,0 +1,75 @@
+﻿#pragma once
+
+#include <chrono>
+#include <thread>
+#include <vector>
+
+#include "SleepSort.h"
+
+struct Widget
+{
+   explicit Widget(unsigned int);
+   unsigned int i;
+};
+
+struct Widget2
+{
+};
+
+template<std::integral T>
+unsigned int intRepr(T a)
+{
+   return static_cast<unsigned>(a);
+}
+unsigned int intRepr(const Widget&);
+unsigned int intRepr(const std::string&);
+
+
+
+template<typename T>
+concept IntRepresentable = requires(T a) {
+   {intRepr(a)} -> std::unsigned_integral;
+};
+
+template<typename T> requires IntRepresentable<T>
+   std::vector<T> sleepSort(const std::vector<T>& source)
+{
+   std::vector<T> outVector;
+   outVector.reserve(source.size());
+
+   std::vector<std::thread> threads;
+   outVector.reserve(threads.size());
+
+   std::mutex vector_mutex;
+
+   std::mutex mutex;
+   std::unique_lock uniqueLock(mutex);
+
+   std::condition_variable conditionVar;
+   bool isReady = false;
+
+   auto processItem = [&vector_mutex, &outVector, &mutex, &conditionVar, &isReady](const T& item) {
+      std::unique_lock lk(mutex);
+      conditionVar.wait(lk, [&isReady] {return isReady; });
+      lk.unlock();
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(intRepr(item) * 10));
+
+      {
+         std::lock_guard lockGuard(vector_mutex);
+         outVector.push_back(item);
+      }
+   };
+
+   for(const auto& item: source)
+      threads.emplace_back([processItem, &item] { return processItem(item); });
+
+   isReady = true;
+   uniqueLock.unlock();
+   conditionVar.notify_all();
+
+   for (auto& t : threads)
+      t.join();
+
+   return outVector;
+}
